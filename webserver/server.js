d513548f153a181bf9cf2c587f80b88c4230dff2
Nicholas Smith <nick@nicholassmith.me.uk>
2012-10-14 15:54:33 +0100

    Modified to use id routes

--- webserver/server.js
+++ webserver/server.js
@@ -1,20 +1,24 @@
-var http = require("http");
-var pages = [
-     {route: '/', output: 'Woohoo!'},
-     {route: '/about/this', output: 'Multilevel routing with Node'},
-     {route: '/about/node', output: 'Evented I/O for V8 JavaScript.'},
-     {route: '/another page', output: function () {return 'Here\'s ' +
-   this.route; }}
+var http = require('http');
+var url = require('url');
+
+var pages = [ 
+	{id: '1', route: ' ', output : 'Woohoo'},
+	{id: '2', route: 'about', output: 'A simple routing with node example'},
+	{id: '3', route: 'another page', output: function() {return 'Here\'s}' + this.route;}},
 ];
+
 http.createServer(function (request, response) {
-	var lookup = decodeURI(request.url);
-	pages.forEach(function(page) {
-		if(page.route == lookup) {
-			response.writeHead(200, {'Content-Type' : 'text/html'});
-			response.end(typeof page.output == 'function' ? page.output() : page.output);
-		}
-	});
-	if(!response.finished) {
+	var id = url.parse(decodeURI(request.url), true).query.id;
+	if (id) {
+		pages.forEach(function(page) {
+			if(page.id == id) {
+				response.writeHead(200, {'Content-Type': 'text/html'});
+				response.end(typeof page.output === 'function' ? page.output() : page.output);
+			}
+		});
+	}
+	if(!response.finished)
+	{
 		response.writeHead(404);
 		response.end('Page Not Found');
 	}
