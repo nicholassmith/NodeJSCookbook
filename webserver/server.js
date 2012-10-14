59cbaef7a684a857a75513a670f515c6ccf70525
Nicholas Smith <nick@nicholassmith.me.uk>
2012-10-14 16:15:03 +0100

    Looking up static files

--- webserver/server.js
+++ webserver/server.js
@@ -1,25 +1,10 @@
 var http = require('http');
-var url = require('url');
-
-var pages = [ 
-	{id: '1', route: ' ', output : 'Woohoo'},
-	{id: '2', route: 'about', output: 'A simple routing with node example'},
-	{id: '3', route: 'another page', output: function() {return 'Here\'s}' + this.route;}},
-];
-
-http.createServer(function (request, response) {
-	var id = url.parse(decodeURI(request.url), true).query.id;
-	if (id) {
-		pages.forEach(function(page) {
-			if(page.id == id) {
-				response.writeHead(200, {'Content-Type': 'text/html'});
-				response.end(typeof page.output === 'function' ? page.output() : page.output);
-			}
-		});
-	}
-	if(!response.finished)
-	{
-		response.writeHead(404);
-		response.end('Page Not Found');
-	}
+var path = require('path');
+var fs = require('fs');
+http.createServer(function(request, response) {
+	var lookup = path.basename(decodeURI(request.url)) || 'index.html',
+		f = 'content/' + lookup;
+	fs.exists(f, function (exists) {
+		console.log(exists ? lookup + " is there" : lookup + " doesn't exist");
+	});
 }).listen(8080);
