cf287cf487ac57cda327fb5cee81e5719ff246ed
Nicholas Smith <nick@nicholassmith.me.uk>
2012-10-14 16:40:18 +0100

    Serving static files

--- webserver/server.js
+++ webserver/server.js
@@ -1,10 +1,29 @@
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
+var mimeTypes = {
+	'.js' : 'text/javascript',
+	'.html' : 'text/html',
+	'.css' : 'text/css'
+};
+//requires variables, mimeType object
 http.createServer(function(request, response) {
 	var lookup = path.basename(decodeURI(request.url)) || 'index.html',
 		f = 'content/' + lookup;
 	fs.exists(f, function (exists) {
-		console.log(exists ? lookup + " is there" : lookup + " doesn't exist");
+		if(exists) {
+			fs.readFile(f, function(err, data) {
+				if(err) { response.writeHead(500);
+					response.end('Server Error!'); 
+					return;
+				}
+				var headers = {'Content-type' : mimeTypes[path.extname(lookup)]};
+				response.writeHead(200, headers);
+				response.end(data);
+			});
+			return;
+		}
+		response.writeHead(404);
+		response.end();
 	});
 }).listen(8080);
