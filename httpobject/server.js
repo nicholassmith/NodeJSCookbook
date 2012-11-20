ab95e6dedcf6634a1be8b9fd484245f81b9ff3e6
Nicholas Smith <nick@nicholassmith.me.uk>
2012-11-20 14:00:58 +0000

    Changing to form uploads

--- httpobject/server.js
+++ httpobject/server.js
@@ -5,7 +5,7 @@ var util = require('util');
 connect(connect.limit('64kb'), connect.bodyParser(), function(request, response) {
 	if(request.method === "POST") {
 		console.log("User posted:\n", request.body);
-		response.end("You posted:\n", util.inspect(request.body));
+		response.end("You posted:\n" + util.inspect(request.body));
 	}
 	if(request.method === "GET") {
 		response.writeHead(200, {'Content-Type' : 'text/html'});
