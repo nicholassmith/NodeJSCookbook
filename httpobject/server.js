b70a9316dc9b2ac2005f2060b53d06ed5eea0bfb
Nicholas Smith <nick@nicholassmith.me.uk>
2012-11-20 15:21:19 +0000

    Server js changes, appears there's some specific issue with this code so leaving it for the moment

--- httpobject/server.js
+++ httpobject/server.js
@@ -1,11 +1,18 @@
 var connect = require('connect')
 var form = require('fs').readFileSync('form.html');
-var util = require('util');
+var formidable = require('formidable');
 
 connect(connect.limit('64kb'), connect.bodyParser(), function(request, response) {
 	if(request.method === "POST") {
-		console.log("User posted:\n", request.body);
-		response.end("You posted:\n" + util.inspect(request.body));
+		var incoming = new formidable.IncomingForm();
+		incoming.uploadDir = "uploads";
+		incoming.on('file', function(field, file) {
+			if(!file.size) {return;}
+			response.write(file.name + " received\n");
+		}).on('end', function () {
+			response.end("All files received");
+		});
+		incoming.parse(request);
 	}
 	if(request.method === "GET") {
 		response.writeHead(200, {'Content-Type' : 'text/html'});
