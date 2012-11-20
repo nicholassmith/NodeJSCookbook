5a0a1f4db2640204a8732b63486693fb73de9d0a
Nicholas Smith <nick@nicholassmith.me.uk>
2012-11-20 12:37:27 +0000

    Swapped to using connect from using http directly

--- httpobject/server.js
+++ httpobject/server.js
@@ -1,29 +1,14 @@
-var http = require('http');
+var connect = require('connect')
 var form = require('fs').readFileSync('form.html');
-var querystring = require('querystring');
 var util = require('util');
-var maxData = 2 * 1024 * 1024;
 
-http.createServer(function(request, response) {
+connect(connect.limit('64kb'), connect.bodyParser(), function(request, response) {
 	if(request.method === "POST") {
-		var postData = '';
-		request.on('data', function(chunk) {
-			postData += chunk;
-			if(postData.length < maxData) {
-				postData = '';
-				this.pause();
-				response.writeHead(413);
-				response.end('Too Large');
-			}
-		}).on('end', function() {
-			if(!postData) {response.end(); return;}
-			var postDataObject = querystring.parse(postData);
-			console.log('User posted:\n' + postData);
-			response.end('You posted:\n' + util.inspect(postDataObject));
-		});
+		console.log("User posted:\n", request.body);
+		response.end("You posted:\n", util.inspect(request.body));
 	}
 	if(request.method === "GET") {
-		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.writeHead(200, {'Content-Type' : 'text/html'});
 		response.end(form);
 	}
 }).listen(8080);
