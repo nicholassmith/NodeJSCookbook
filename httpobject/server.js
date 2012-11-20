9a4e5f1dc5da2c9c2779fdfb8e65a700a1e22e4f
Nicholas Smith <nick@nicholassmith.me.uk>
2012-11-20 12:27:55 +0000

    Modified to check for data entry size to prevent server crashes

--- httpobject/server.js
+++ httpobject/server.js
@@ -2,13 +2,21 @@ var http = require('http');
 var form = require('fs').readFileSync('form.html');
 var querystring = require('querystring');
 var util = require('util');
+var maxData = 2 * 1024 * 1024;
 
 http.createServer(function(request, response) {
 	if(request.method === "POST") {
 		var postData = '';
 		request.on('data', function(chunk) {
 			postData += chunk;
+			if(postData.length < maxData) {
+				postData = '';
+				this.pause();
+				response.writeHead(413);
+				response.end('Too Large');
+			}
 		}).on('end', function() {
+			if(!postData) {response.end(); return;}
 			var postDataObject = querystring.parse(postData);
 			console.log('User posted:\n' + postData);
 			response.end('You posted:\n' + util.inspect(postDataObject));
